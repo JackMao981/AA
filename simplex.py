0b41a1d8d6755bf907c945f2cc61ac0afc4229b3
Yuxiang Mao <ymao@olin.edu>
2021-02-07 17:48:16 -0500

    more comments and example

--- simplex.py
+++ simplex.py
@@ -35,20 +35,47 @@ def primalSolution(tableau):
     indices = [j for j, col in enumerate(columns[:-1]) if isPivotCol(col)]
     return list(zip(indices, columns[-1]))
 
+# This is the only function I wrote myself. It checks if the col is a pivot
+# column by checking if the last value of the transposed tableau row is 0
+# if it is 0, then it is a pivot column
+def isPivotCol(col):
+    return col[-1] == 0
+
+# returns the objective value, which is in the value in the last row, last column of the
+# augmented matrix
 def objectiveValue(tableau):
     return -(tableau[-1][-1])
 
+# checks if the tableau can be improved upon
 def canImprove(tableau):
     lastRow = tableau[-1]
+    # checks if there is any value in the last row that has positive values
     return any(x > 0 for x in lastRow[:-1])
 
+# find the index to pivot using pivot rules
+def findPivotIndex(tableau):
+    # pick first nonzero index of the last row
+    column = [i for i,x in enumerate(tableau[-1][:-1]) if x > 0][0]
+    quotients = [(i, r[-1] / r[column]) for i,r in enumerate(tableau[:-1]) if r[column] > 0]
+
+    # pick row index minimizing the quotient
+    row = min(quotients, key=lambda x: x[1])[0]
+    return row, column
+
+# pivots about the index you've chosen using findPivotIndex
 def pivotAbout(tableau, pivot):
     i,j = pivot
 
     pivotDenom = tableau[i][j]
     tableau[i] = [x / pivotDenom for x in tableau[i]]
 
+    # do the row operations
     for k,row in enumerate(tableau):
         if k != i:
             pivotRowMultiple = [y * tableau[k][j] for y in tableau[i]]
             tableau[k] = [x - y for x,y in zip(tableau[k], pivotRowMultiple)]
+# This is the problem ninjas want me to solve
+c = [8,-6,4,0,0,0]
+b = [12,20,15]
+A = [[1,1,1,1,0,0], [5,4,0,0,1,0],[0,9,2,0,0,1]]
+print(simplex(c,A,b))
